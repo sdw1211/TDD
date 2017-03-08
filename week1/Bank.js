010c7fbd847fc2649e4868003b7c3f522c6674b0
sdw1211 <sdw1211@gmail.com>
2017-03-08 17:42:21 +0900

    2주차 추가 수정

--- week1/Bank.js
+++ week1/Bank.js
@@ -8,13 +8,17 @@ class Bank {
         this.rate = new Map();
     }
 
+    makeString(from, to) {
+        return `${from}->${to}`;
+    }
+
     addRate(from, to, rate) {
-        this.rate.set(`${from}->${to}`, rate);
+        this.rate.set(this.makeString(from, to), rate);
     }
 
     getRate(from, to) {
-        if (this.rate.has(`${from}->${to}`)) {
-            return this.rate.get(`${from}->${to}`);
+        if (this.rate.has(this.makeString(from, to))) {
+            return this.rate.get(this.makeString(from, to));
         } else {
             return 1;
         }
